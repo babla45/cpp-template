137b3d732911a45d599cc2b07ad81a20e68a511c
Md Babla Islam <134688680+babla45@users.noreply.github.com>
2024-07-06 08:18:46 +0600

    Add files via upload

---
+++ cpp_for_cp.cpp
@@ -0,0 +1,73 @@
+//.....b_a_b_l_a......
+#include<bits/stdc++.h>
+using namespace std;
+#ifndef onliine
+#define b_i_b       ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define pam(x)      for(auto ww:x) cout<<ww.ft<<sp<<ww.sd<<nl;
+#define file        freopen("input.txt","r",stdin);\
+                    freopen("output.txt","w",stdout);
+#define pa(x)       for(auto ww:x) cout<<ww<<sp;
+#define ia(v)       for(auto &x:v) cin>>x;
+#define rforr(x,y)  for(int a=y-1;a>=x;a--)
+#define forr(x,y)   for(int a=x; a<y; a++)
+#define mem(a,x)    memset(a,x,sizeof(a));
+#define pyesr       cout<<"YES\n";return;
+#define pnor        cout<<"NO\n";return;
+#define bss(x,k)    bs(all(x),k)
+#define all(x)      x.bn,x.ed
+#define vs          vector<string>
+#define bs          binary_search
+#define vc          vector<char>
+#define vi          vector<int>
+#define vll         vector<ll>
+#define ub          upper_bound
+#define lb          lower_bound
+#define pb          push_back
+#define bn          begin()
+#define ed          end()
+#define nl          "\n"
+#define sp          " "
+#define ft          first
+#define sd          second
+#define sz          size()
+#define nll         cout<<"\n";
+#define spc         cout<<" ";
+#define pno         cout<<"NO\n";
+#define pyes        cout<<"YES\n";
+#define pr(x)       cout<<x<<"\n";
+#define pd(x)       cout<<x<<" ";
+#define ll          long long
+#define mii         map<int,int>
+#define mci         map<char,int>
+#define msi         map<string,int>
+#define vpii        vector<pair<int,int>>
+#define rall(x)     x.rbegin(),x.rend()
+#define vpci        vector<pair<char,int>>
+#define vpsi        vector<pair<string,int>>
+#define pp(x)       cerr<<(#x)<<"="<<x<<"\n";
+#define __unique(s) s.resize(unique(all(s))-s.bn);
+#endif
+
+
+// !******************************************************************!
+// !*************************Code Begins Here*************************!
+// !******************************************************************!
+
+
+void solve()
+{
+    
+}
+
+
+int main()
+{
+    file
+    //solve2(); return 0;
+    b_i_b int t=1;
+    cin>>t;
+    while(t--)
+    solve();
+     
+    return 0;
+}
